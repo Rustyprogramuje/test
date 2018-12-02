8529471473ab84a328e892982acb58083c3064c9
rustyprogramuje@gmail.com <Dea345SamWichester>
2018-12-02 19:27:56 +0100

    Layout bmi dodany

--- cosiedzieje/app/src/main/java/com/example/karolina/cosiedzieje/startPage.kt
+++ cosiedzieje/app/src/main/java/com/example/karolina/cosiedzieje/startPage.kt
@@ -2,24 +2,29 @@ package com.example.karolina.cosiedzieje
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_start_page.*
 
 class startPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_page)
+        setContentView(R.layout.bmi)
 
         val liczby = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
         val suma = liczby.sum()
-       // tekst.text = suma.toString()
+        // tekst.text = suma.toString()
 
-        var mojebmi = BMI(
-            1.72,
-            80.0
-        )
-        mojebmi.obliczbmi()
-       bmitekst.text = mojebmi.bmi.toString()
+        val klik = findViewById<Button>(R.id.klik)
+        klik.setOnClickListener {
+            var mojebmi = BMI(
+                1.72,
+                80.0
+            )
+            mojebmi.obliczbmi()
+            bmitekst.text = mojebmi.bmi.toString()
+
+        }
 
     }
 }
