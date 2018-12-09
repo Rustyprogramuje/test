d0f216b00a29c5450bb031af085bfe6c9ddf5a1a
rustyprogramuje@gmail.com <Dea345SamWichester>
2018-12-09 18:24:37 +0100

    Otwierajace się różne activity z okna startPage.kt

--- cosiedzieje/app/src/main/java/com/example/karolina/cosiedzieje/startPage.kt
+++ cosiedzieje/app/src/main/java/com/example/karolina/cosiedzieje/startPage.kt
@@ -1,30 +1,50 @@
 package com.example.karolina.cosiedzieje
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_start_page.*
+import kotlinx.android.synthetic.main.bmi.*
 
 class startPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.bmi)
-
-        val liczby = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        setContentView(R.layout.activity_start_page)
+        // dzialająca reakcja na klikniecie buttona, nie
+        // uwzględnia na razie podawania danych
+        /*val liczby = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
         val suma = liczby.sum()
-        // tekst.text = suma.toString()
+        // tekst.text = suma.toString()*/
 
-        val klik = findViewById<Button>(R.id.klik)
+        /*val klik = findViewById<Button>(R.id.klik)
         klik.setOnClickListener {
             var mojebmi = BMI(
                 1.72,
                 80.0
             )
             mojebmi.obliczbmi()
-            bmitekst.text = mojebmi.bmi.toString()
+            bmitekst1.text = mojebmi.bmi.toString()
 
+        }*/
+        btn.setOnClickListener {
+            val intent = Intent (this, activ::class.java)
+        startActivity(intent)
         }
 
+        btn2.setOnClickListener {
+            val intent = Intent (this, HPHouses2::class.java)
+            startActivity(intent)
+
+        }
+        btn3.setOnClickListener {
+            val intent = Intent (this, ObliczBMI::class.java)
+            startActivity(intent)
+        }
+        btn4.setOnClickListener {
+            val intent = Intent (this, MiniInsta_act::class.java)
+            startActivity(intent)
+        }
     }
 }
