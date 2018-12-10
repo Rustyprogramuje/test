34730387c6e5be1b7e47876190dd8f7ca49a0269
rustyprogramuje@gmail.com <Dea345SamWichester>
2018-12-10 22:44:50 +0100

    dodane bmi - dodawanie tekstu i używanie do obliczeń

--- cosiedzieje/app/src/main/java/com/example/karolina/cosiedzieje/ObliczBMI.kt
+++ cosiedzieje/app/src/main/java/com/example/karolina/cosiedzieje/ObliczBMI.kt
@@ -3,6 +3,8 @@ package com.example.karolina.cosiedzieje
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import kotlinx.android.synthetic.main.bmi.*
 
@@ -12,18 +14,50 @@ class ObliczBMI : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bmi)
 
-        var kliknij = findViewById<Button>(R.id.kliknij)
+        var waga1 = 0.0
+        wpiszwage.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val waga = s.toString()
+                waga1 = waga.toDouble()
+            }
+        })
+
+        var wzrost1 = 0.0
+        wpiszwzrost.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val wzrost = s.toString()
+                wzrost1 = wzrost.toDouble()
+            }
+        })
+
+        val kliknij = findViewById<Button>(R.id.kliknij)
         kliknij.setOnClickListener {
-            var mojebmi = BMI(
-                1.72,
-                80.0
+            val mojebmi = BMI(
+                wzrost1,
+                waga1
             )
             mojebmi.obliczbmi()
             bmitekst.text = mojebmi.bmi.toString()
 
         }
         wroc.setOnClickListener {
-            val intent = Intent (this, startPage::class.java)
+            val intent = Intent(this, startPage::class.java)
             startActivity(intent)
         }
 
